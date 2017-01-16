291624814e908fe0f76aba7f733e4cf2ca257ec8
ds-xiao <qifu.xiao@outlook.com>
2017-01-16 10:47:42 +0800

    version to AgCopter-v1.2.1

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "AgCopter V1.2.0"
-#define FIRMWARE_VERSION 1,2,0,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define THISFIRMWARE "AgCopter V1.2.1"
+#define FIRMWARE_VERSION 1,2,1,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 /*
    This program is free software: you can redistribute it and/or modify
